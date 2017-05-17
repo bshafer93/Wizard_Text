f4fc13f6663fa105393fcccc7b64fedfef248f12
bshafer93 <bshafer93@gmail.com>
2017-05-17 15:31:45 -0700

    user Requests!

--- go/loginPlane.go
+++ go/loginPlane.go
@@ -36,5 +36,17 @@ func main() {
 
 // Handles incoming requests.
 func handleRequest(conn net.Conn) {
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	reqLen, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
 
+	fmt.Println(reqLen)
+	// Send a response back to person contacting us.
+	conn.Write([]byte("Message received."))
+	// Close the connection when you're done with it.
+	conn.Close()
 }
