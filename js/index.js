94d99ca550987e3321f8c08ca43dafed0dae98ed
bshafer93 <bshafer93@gmail.com>
2017-05-20 00:51:45 -0700

    Adding Error catching, updated a bunch athings since last time too

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 var $ = require('jQuery');
 var net = require('net');
+var tls = require('tls');
 $(document).ready(function() {
-
-    var client = new net.Socket();
-    client.connect(3333, '107.170.196.189', function() {
+    var client = new tls.TLSSocket();
+    client.connect(3333, '192.168.0.25', function () {
 
         console.log('Connected to the server!');
+        addToChat("You are now connected to the server! Welcome :)");
+        addToChat("Please type /Register to make a account. or type /Login to Login:");
     });
+
 
     client.on('data', function(data) {
         console.log('Received: ' + data);
         addToChat(data);
     });
-
-
-
 
     client.on('close', function() {
         console.log('Connection closed');
@@ -25,20 +25,22 @@ $(document).ready(function() {
         console.log('Ready!');
     });
 
-    sendMessage();
+    client.on('error', (err) => {
+        addToChat("Connection failed, restart the game please :)");
+        console.error(err);
+    });
 
+    sendMessage();
 
     function sendMessage() {
         $("#submitLine").keypress(function(e) {
             if (e.keyCode === 13) {
                 var userMsg = $('#submitLine').text();
                 console.log(userMsg);
-                client.write("heartbeat" + "\n");
                 client.write(userMsg + "\n");
                 $('#submitLine').text('');
                 document.execCommand('insertHTML', false, '');
                 return false;
-
             }
         });
 
@@ -49,10 +51,6 @@ $(document).ready(function() {
         var content = "<div id=chatLogMsg>OtherUser>" + msg + "</div>";
 
         $("#messageField").prepend(content);
-
-
     }
-
-
 
 });
